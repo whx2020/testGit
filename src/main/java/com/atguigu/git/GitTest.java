ecd2f961fb206ea638822e519cebf4290a6212af
whx <403331295@qq.com>
2022-05-06 21:48:20 +0800

    push test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hot-fix test!!");
         System.out.println("master test!!");
         System.out.println("hot-fix test2!!");
+        System.out.println("pust test!!");
     }
 
 }
